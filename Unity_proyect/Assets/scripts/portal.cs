65ed899cf485c0728fc54e082ec205838f6bb351
LordMascachapas <alfredm3007@gmail.com>
2018-10-20 12:24:39 +0200

    Portal prefabs and script

---
+++ Unity_proyect/Assets/scripts/portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portal : MonoBehaviour
+{
+    GameObject OtherPortal;
+
+    private void Awake()
+    {
+        OtherPortal = GameObject.Find("PortalExit");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.position = OtherPortal.transform.position;
+        }
+    }
+}
