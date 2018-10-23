68f4a744f43d40f5a6eae4964cefa257326664ca
LordMascachapas <alfredm3007@gmail.com>
2018-10-23 12:01:19 +0200

    portalSpawner follow the mouse

---
+++ Unity_proyect/Assets/scripts/portalSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalSpawner : MonoBehaviour {
+    public GameObject PortalEnterPrefab;
+    public GameObject PortalExitPrefab;
+    bool aviableToShoot;
+    string actualState;
+
+    private void Start()
+    {
+        actualState = "portalEnter";
+        aviableToShoot = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Bouncer") || collision.gameObject.CompareTag("Ground"))
+            aviableToShoot = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        aviableToShoot = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && aviableToShoot)
+        {
+            if(actualState == "portalEnter")
+            {
+                actualState = "portalExit";
+                Instantiate(PortalEnterPrefab, transform.position, Quaternion.Euler(0, 0, 0));
+            }
+            else if(actualState == "portalExit")
+            {
+                actualState = "portalEnter";
+                Instantiate(PortalExitPrefab, transform.position, Quaternion.Euler(0, 0, 0));
+            }
+        }
+    }
+}
