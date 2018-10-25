3fc974f19f18529af288b54a08679869b0689d0a
LordMascachapas <alfredm3007@gmail.com>
2018-10-25 12:19:30 +0200

    Created and implemented a script which move the element with a constant velocity

---
+++ Unity_proyect/Assets/scripts/worldScroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldScroller : MonoBehaviour
+{
+
+    Rigidbody2D rigOfMine;
+    void Awake()
+    {
+        rigOfMine = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rigOfMine.velocity = Vector2.left * gameController.Instance.XWorldScrollImpulse;
+    }
+}
